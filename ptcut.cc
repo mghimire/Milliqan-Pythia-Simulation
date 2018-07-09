2981031c74db35be1e3da3afc7624368f91dd642
GBeauregard <GBeauregard@users.noreply.github.com>
2018-07-09 14:19:59 -0400

    cleanup

--- ptcut.cc
+++ ptcut.cc
@@ -12,14 +12,16 @@ void ptcut(Double_t pTcut = 0.0, Int_t nevents = 1000,
   unsigned int nentries = sourceTree->GetEntries();
   Double_t pT = 0.0;
   sourceTree->SetBranchAddress("pT", &pT);
-  // count how many muons pass a pT cut (in GeV)
-  int num_passed_cut = 0;
+  // count how many mCP pass a pT cut (in GeV)
+  int num_passcut = 0;
   for (unsigned int i = 0; i < nentries; i++) {
     sourceTree->GetEntry(i);
-    if (pT >= pTcut) num_passed_cut++;
+    if (pT >= pTcut) num_passcut++;
   }
-  // cout.precision(10);
-  cout << num_passed_cut << " muons passed pT cut of " << pTcut << endl;
-  cout << num_passed_cut / (1. * nevents) << "+-"
-       << TMath::Sqrt(num_passed_cut) / (1. * nevents) << " mCP/event" << endl;
+  // output number of mCP that pass the pT cut
+  cout << num_passcut << " mCP passed pT cut of " << pTcut << " GeV" << endl;
+  // calculate mCP/event that pass the pT cut
+  cout << num_passcut / (1. * nevents) << "+-"
+       << TMath::Sqrt(num_passcut) / (1. * nevents) << " mCP/event pass cut"
+       << endl;
 }
