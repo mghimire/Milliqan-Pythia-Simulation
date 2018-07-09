a016857c790aace376dfdaa95b920aa471e9ff63
GBeauregard <GBeauregard@users.noreply.github.com>
2018-07-09 12:29:10 -0400

    fixed style

--- ptcut.cc
+++ ptcut.cc
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <TFile.h>
 #include <TROOT.h>
 #include <TTree.h>
-#include <TFile.h>
+#include <iostream>
 
-void ptcut(Double_t pTcut=0.0) {
+void ptcut(Double_t pTcut = 0.0) {
   // read in output file
   TFile *EventFile = new TFile("out.root", "READ");
   TTree *sourceTree = (TTree *)EventFile->Get("EventTree");
@@ -14,7 +14,7 @@ void ptcut(Double_t pTcut=0.0) {
   int num_passed_cut = 0;
   for (unsigned int i = 0; i < nentries; i++) {
     sourceTree->GetEntry(i);
-    if (pT>=pTcut) num_passed_cut++;
+    if (pT >= pTcut) num_passed_cut++;
   }
   cout << num_passed_cut << " muons passed pT cut of " << pTcut << endl;
 }
