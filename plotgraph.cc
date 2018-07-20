d9c746c41b8c20dbd4128fbc08dabf8d97e2192c
GBeauregard <GBeauregard@users.noreply.github.com>
2018-07-20 09:53:33 -0400

    initial commit of plotgraph

---
+++ plotgraph.cc
@@ -0,0 +1,59 @@
+#include <TFile.h>
+#include <TMath.h>
+#include <TROOT.h>
+#include <TTree.h>
+#include <TVectorD.h>
+#include <iostream>
+#include <vector>
+#include "ptcut.cc"
+
+// load ptcut.cc macro to do analysis as mCP_anal struct defined there
+void plotgraph(Double_t pTcut = 0.0) {
+  // load up vectors of the masses we have files for and the number of files of each mass
+  std::vector<TString> masses;
+  std::vector<int> num_files;
+
+  masses.push_back("0.01");
+  num_files.push_back(2);
+
+  masses.push_back("0.0540");
+  num_files.push_back(2);
+
+  masses.push_back("0.0810");
+  num_files.push_back(2);
+
+  masses.push_back("0.219");
+  num_files.push_back(0);
+
+  masses.push_back("0.329");
+  num_files.push_back(0);
+
+  masses.push_back("0.5");
+  num_files.push_back(0);
+
+  masses.push_back("1");
+  num_files.push_back(0);
+
+  masses.push_back("2");
+  num_files.push_back(0);
+
+  masses.push_back("10");
+  num_files.push_back(0);
+
+  std::vector<vector<TString>> files;
+  // create lists of file names
+  for(std::size_t i=0; i<masses.size(); i++) {
+    std::vector<TString> mass_files;
+    for(int j=1; j<=num_files[i]; j++) {
+      mass_files.push_back("data/" + masses[i] + "_" + j + ".root");
+    }
+    files.push_back(mass_files);
+  }
+
+  // run analysis on each file
+  std::vector<mCP_anal> analyses;
+  for (std::size_t i=0; i<files.size(); i++) {
+    mCP_anal analysis=analyze_pythia_sim(pTcut,files[i]);
+    analyses.push_back(analysis);
+  }
+}
--- ptcut.cc
+++ ptcut.cc
@@ -48,9 +48,9 @@ mCP_anal analyze_pythia_sim(Double_t pTcut = 0.0, std::vector<TString> infiles =
   double sum_invtree_weight_err_sq = 0.0;
 
   // loop through input root files
-  for (TString infile : infiles) {
+  for (std::size_t f=0; f<infiles.size(); f++) {
     // read in input file
-    TFile *EventFile = new TFile(infile, "READ");
+    TFile *EventFile = new TFile(infiles[f], "READ");
     TTree *sourceTree = (TTree *)EventFile->Get("EventTree");
     unsigned int nentries = sourceTree->GetEntries();
     // Get event tree weight that includes cross section and number of events.
