27c69aaea1fe0076ccb5dfcb04dad6a863e00ce0
GBeauregard <GBeauregard@users.noreply.github.com>
2018-07-20 07:01:33 -0400

    update script to spawn runs

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 source ~/.profile
 mkdir -p ./data
-declare -a masses=("2"
-                   "1"
-                   "0.5"
+declare -a masses=("0.5"
                    "0.329"
                    "0.219"
-                   "0.0810"
-                   "0.0540"
-                   "0.01"
                    )
-NUM_FILES=3
+NUM_START=1
+NUM_END=2
 EVENTS_PER_FILE=5000000
 for mass in "${masses[@]}"
 do
-  for ((i=1;i<=NUM_FILES;i++)); do
+  for ((i=NUM_START;i<=NUM_END;i++)); do
     screen -S ${mass//.}_${i} -d -m sh -c "source ~/.profile;./py_sim -p 0 -m ${mass} -n ${EVENTS_PER_FILE} -f data/${mass}_${i}.root"
     # tmux new-session -d -s ${mass//.}_${i} "./py_sim -p 0 -m ${mass} -n ${EVENTS_PER_FILE} -f data/${mass}_${i}.root"
   done
