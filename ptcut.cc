c20da891d5ef74f1b7a891c952169e6551ecc4eb
GBeauregard <GBeauregard@users.noreply.github.com>
2018-09-19 10:20:37 -0400

    actually fixed milliqan eta

--- ptcut.cc
+++ ptcut.cc
@@ -29,15 +29,23 @@ mCP_anal analyze_pythia_sim(Double_t pTcut = 0.0, Double_t charge = 1e-3,
   mCP_anal analysis;
 
   // calculate eta/phi acceptance
-  Double_t det_loc = 6 * TMath::Pi() / 180.0;  // detector at 6 deg eta
+  Double_t det_loc = 84.0 * TMath::Pi() / 180.0;  // detector at th=84 deg 
   Double_t det_width = 1.0;                    // detector is 1x1 m
   Double_t det_dis = 33.0;                     // detector dist 33 m
   Double_t angle_sub = det_width / det_dis;    // angle subtended by detector
   Double_t phi_acceptance = angle_sub / (2 * TMath::Pi());  // phi acceptance
   // calculate eta with width extra_width times larger for good stats
-  Double_t extra_width = 1.0;
+  Double_t extra_width = 6.0;
+  Double_t low_the = det_loc + 0.5 * angle_sub * extra_width;
+  low_the*=180.0/TMath::Pi();
   Double_t low_eta = calc_eta(det_loc + 0.5 * angle_sub * extra_width);
+  cout<<"low the:"<<low_the<<endl;
+  cout<<"low eta:"<<low_eta<<endl;
+  Double_t high_the = det_loc - 0.5 * angle_sub * extra_width;
+  high_the*=180.0/TMath::Pi();
   Double_t high_eta = calc_eta(det_loc - 0.5 * angle_sub * extra_width);
+  cout<<"hig the:"<<high_the<<endl;
+  cout<<"hig eta:"<<high_eta<<endl;
 
   // initialize variables
   double event_sum = 0.0;
