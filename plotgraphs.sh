a066250f6844bee9e6dd03282a95962d6e7c7618
Mukesh Ghimire <mg5213@pd334.physics.nyu.edu>
2019-04-29 15:29:07 -0400

    modified plotgraphs.sh for new modes

--- plotgraphs.sh
+++ plotgraphs.sh
@@ -1,17 +1,20 @@
+#wget https://www.dropbox.com/s/q76sfinwimys77j/data14pt2.zip
+#unzip data14pt2.zip
+#rm data14pt2.zip
 
 dohist=0 # make debugging histograms
 
-root -l -q plotgraph.cc\(\"gammaZ$1\",$dohist\)
+root -l -q plotgraph.cc\(\"gammaZ$1\",$2,$3,$dohist\)
 sleep 1
-root -l -q plotgraph.cc\(\"onia$1\",$dohist\)
+root -l -q plotgraph.cc\(\"onia$1\",$2,$3,$dohist\)
 sleep 1
-root -l -q plotgraph.cc\(\"qcd$1\",$dohist\)
+root -l -q plotgraph.cc\(\"qcd$1\",$2,$3,$dohist\)
 sleep 1
 
-nlay=4 # 1 (full eff), 3, or 4
+nlay=1 # 1 (full eff), 3, or 4
 root -l -q filleff.cc\($nlay\)
 
 doqcd=1 # 0 or 1
 bkg=0 # 0 (optimistic) or 1 (orig)
 
-root -l -q plots/heatplotpTweight_qcd$1.C plots/heatplotpTweight_onia$1.C plots/heatplotpTweight_gammaZ$1.C plots/eff${nlay}.C limitplot.cc\($doqcd,$bkg,\"$1\"\)
+root -l -q plots/heatplotpTweight_type$2\_rock$3\_qcd$1.C plots/heatplotpTweight_type$2\_rock$3\_onia$1.C plots/heatplotpTweight_type$2\_rock$3\_gammaZ$1.C plots/eff${nlay}.C limitplot.cc\($doqcd,$bkg,\"$1\"\,$2,$3\)
